3eb581e00cd3205c53a962aafa2e356f4bfb1d34
Jose Dominguez <itjosedominguez@gmail.com>
2023-03-07 23:37:34 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region                      = "us-east-1"
-  s3_use_path_style           = false
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   
   ####### se cambio s3_force_path_style por s3_use_path_style	################
-  s3_use_path_style = true
+  s3_use_path_style = false
   endpoints {
     s3 = "172.17.0.3:4566"
   }
